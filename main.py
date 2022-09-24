9b8dceb2e7b1d7271533ca402db42098c8958720
idoandify16 <idoandify16@gmail.com>
2022-09-24 19:27:51 +0700

    Menambahkan fungsi initialize

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ import json
 
 st.set_page_config(layout='wide')
 
-# json_data = st.secrets["json_data"]
-# service_account = st.secrets["service_account"]
+json_data = st.secrets["json_data"]
+service_account = st.secrets["service_account"]
+
+@st.experimental_memo
+def Initialize():
+    # Preparing values
+    json_object = json.loads(json_data, strict=False)
+    service_account = json_object['client_email']
+    json_object = json.dumps(json_object)
+    # Authorising the app
+    credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
+    ee.Initialize(credentials)
+
+Initialize()
 
 # @st.experimental_memo
 # def Initialize():
-#     # Preparing values
-#     json_object = json.loads(json_data, strict=False)
-#     service_account = json_object['client_email']
-#     json_object = json.dumps(json_object)
-#     # Authorising the app
-#     credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
-#     ee.Initialize(credentials)
+#     ee.Initialize()
 
 # Initialize()
-@st.experimental_memo
-def Initialize():
-    ee.Initialize()
-
-Initialize()
 
 
 st.title('Interactive Map')
